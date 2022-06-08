c294724a31eeeb404f570fde9a12a36434c970a5
MALISIUS <106853330+M1R47-T3RMUX@users.noreply.github.com>
2022-06-07 20:34:42 -0500

    Add files via upload

---
+++ instalandor.sh
@@ -0,0 +1,31 @@
+#!/system/bin/sh
+#codigi colombiano 💛💙❤️
+#codigi colombiano 💛💙❤️
+#codigi colombiano 💛💙❤️
+#codigi colombiano 💛💙❤️
+# amo a ni patria
+
+echo instalando.
+sleep 0.5
+clear
+echo instalando..
+sleep 0.5
+clear
+echo instalando...
+sleep 0.5
+clear
+echo instalando.
+sleep 0.5
+clear
+echo instalando..
+sleep 0.5
+clear
+echo instalando...
+clear
+pkg install wget -y
+clear
+wget https://github.com/M1R47-T3RMUX/M1R47-T3RMUX/blob/main/ipmap.sh
+clear
+chmod ipmap.sh
+clear
+bash ipmap.sh
